903af56a2fc595352346289a11f6939e96999c0f
KariSpace <kpivtorak55@gmail.com>
2020-10-24 22:31:49 +0300

    Created IntegerStack class

--- stack.py
+++ stack.py
@@ -21,27 +21,62 @@ class Stack:
     def __str__(self):
         return str(list(iter(self)))
 
+
+class IntegerStack:
+    def __init__(self):
+        self.elements = []
+
+    def push(self, item):
+        try:
+            int(item)
+            self.elements.append(item)
+        except:
+            print("not a string")
+            pass
+
+
+    def pop(self):
+        if not (self.isEmpty()):
+            return self.elements.pop()
+        else:
+            print("stack already empty")
+            return self.elements
+
+    def isEmpty(self):
+        return len(self.elements) == 0
+
+    def __iter__(self):
+        return reversed(self.elements)
+
+    def __str__(self):
+        return str(list(iter(self)))
+
+
 def main():
 
-    my_stack = Stack()
+    my_stack = IntegerStack()
 
-    my_stack.push('a')
-    my_stack.push('b')
+    print('try to push differ values')
+    my_stack.push(45)
+    my_stack.push('79')
     my_stack.push('c')
+    my_stack.push('0.23')
+
+    print(my_stack)
+    print('try to pop')
+    my_stack.pop()
+    my_stack.pop()
+    my_stack.pop()
 
     print(my_stack)
 
-    my_stack.pop()
-    my_stack.pop()
-    my_stack.pop()
-
-    print(my_stack)
-    if my_stack:
-        print("stack not empty")
-    my_stack.pop()
 
     if my_stack.isEmpty():
         print("stack empty")
+    else:
+        print("stack not empty")
+    print(my_stack)
+
 
 if __name__ == '__main__':
     main()
